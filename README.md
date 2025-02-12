5f9db4200ea4579ce0fd2f2e6af903ce0a20b40d
MinhPhuc2k3 <leminhphuc349@gmail.com>
2025-02-12 23:30:55 +0700

    ex2

---
+++ README.md
@@ -0,0 +1,13 @@
+**Exercise 1**
+
+**Output**:  
+
+![screenshot](./output.png)
+
+Step 1: install python
+
+Step 2: open folder in vscode
+
+Step 3: run sql_extractor.py
+
+Step 4: open io.yaml to see output
---
+++ sql_extractor.py
@@ -0,0 +1,48 @@
+import re
+inputs = dict()
+outputs = dict()
+
+print('opening file sql_file.sql')
+f = open('./sql_file.sql', 'r')
+
+
+file_content = f.read().lower()
+pattern = '[a-z0-9_]+\\.[a-z0-9_]+\\.[a-z0-9_]+'
+statements = ['create table', 'insert into', 'update']
+drop_table_statement = 'drop table'
+last_index = 0
+inset  = 1
+
+print('processing...')
+list_tables = re.findall(pattern, file_content)
+for table in list_tables:
+    last_index = file_content.find(table, last_index)
+    before_str = file_content[last_index-15:last_index-1]
+    is_ouput = False
+    for output_statement in statements:
+        if before_str.find(output_statement) != -1:
+            outputs[table] = inset
+            is_ouput = True
+            break
+    if is_ouput == False:
+        inputs[table] = inset
+    if before_str.find(drop_table_statement) != -1:
+        inputs[table] = 1- inset
+        outputs[table] = 1 - inset
+f.close()
+print('sql_file.sql has been closed')
+
+print('opening io.yaml')
+yaml_file = open('./io.yaml', 'w+')
+yaml_file.writelines('inputs:\n')
+for line in inputs.keys():
+    if inputs[line] == 1:
+        yaml_file.writelines(' - '+line+'\n')
+yaml_file.writelines('outputs:\n')
+for line in outputs.keys():
+    if outputs[line] == 1:
+        yaml_file.writelines(' - '+line+'\n')
+yaml_file.close()
+print('io.yaml has been closed')
+
+print('Sucessfull!')
